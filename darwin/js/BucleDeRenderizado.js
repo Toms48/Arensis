bc49db47013a858d3d3a412fb11f9ac043de5492
Ézhor Malkávar <ezhormalkavar@gmail.com>
2016-11-10 09:58:58 +0100

    Darwin modulado, muertes añadidas

---
+++ darwin/js/BucleDeRenderizado.js
@@ -0,0 +1,51 @@
+//BucleDeRenderizado
+    function loop(){
+      
+	  //Dibuja el fondo
+      ctx.fillStyle = "#FFFFFF";
+      ctx.fillRect(0,0,800,800);
+
+      //Para cada individuo
+      for(var i=0; i<=individuos.length-1; i++){
+		
+        //Define la forma del individuo
+        ctx.beginPath();
+        ctx.arc(individuos[i].x, individuos[i].y, individuos[i].tamano, 0, 2*Math.PI);
+
+        //Define su color
+        ctx.fillStyle = "rgb("+Math.round(individuos[i].velocidad*255/100)+","+Math.round(individuos[i].vida*255/100)+","+Math.round(individuos[i].descendencia*255/100)+")";
+
+        //Muestra el individuo
+        ctx.fill();
+        ctx.stroke();
+
+        //Actualiza la posición del individuo
+        individuos[i].x += individuos[i].vx/5;
+        individuos[i].y += individuos[i].vy/5;
+
+        //Comprueba y activa colisiones con las paredes
+        if(individuos[i].x>=500){
+          individuos[i].x = 500;
+          individuos[i].vx *= -1;
+        }
+        if(individuos[i].x<=0){
+          individuos[i].x = 0;
+          individuos[i].vx *= -1;
+        }
+        if(individuos[i].y>=500){
+          individuos[i].y = 500;
+          individuos[i].vy *= -1;
+        }
+        if(individuos[i].y<=0){
+          individuos[i].y = 0;
+          individuos[i].vy *= -1;
+        }
+		
+		//Comprobar muerte
+		if(individuos[i].muerte == Math.floor(Date.now()/1000)){
+			individuos.splice(i, 1);
+		}
+
+		
+      }
+    }
---
+++ darwin/js/CrearIndividuos.js
@@ -0,0 +1,77 @@
+	/*Individuo {x, y, vx, vy, tamaño, velocidad, tiempo de vida, descendencia}
+    Cada variable puede tomar valores entre 0 y 100 (excepto x e y)
+    La suma de todas las variables (excepto x e y) es igual a 100
+    */
+	
+	var atributo = 1;
+	
+	var nTamano;
+    var nVelocidad;
+    var nVida;
+    var nDescendencia;
+    var nvx;
+    var nvy;
+	
+	
+function crearIndividuos(numeroDeIndividuos){
+    
+	//Asignar atributos aleatorios
+    for(var adan=0; adan<=numeroDeIndividuos-1; adan++){
+      nTamano = 0;
+      nVelocidad = 0;
+      nVida = 0;
+      nDescendencia = 0;
+
+      while( (nTamano+nVelocidad+nVida+nDescendencia) < 100 ){
+        switch (atributo) {
+          case 1:
+            nTamano += Math.round(10*Math.random());
+            atributo++;
+            break;
+          case 2:
+            nVelocidad += Math.round(10*Math.random());
+            atributo++;
+            break;
+          case 3:
+            nVida += Math.round(10*Math.random());
+            atributo++;
+            break;
+          case 4:
+            nDescendencia += Math.round(10*Math.random());
+            atributo++;
+            break;
+          case 5:
+            atributo = 1;
+            break;
+
+        }
+      }
+        //Generar un vector unitario aleatorio para el movimiento
+        //Se multiplica por (Math.round(Math.random())*2-1) para que el número sea positivo o negativo aleatoriomente (lo multiplica por -1 o +1)
+        nvx = Math.random() * (Math.round(Math.random())*2-1);
+        nvy = Math.sqrt(1-nvx*nvx) * (Math.round(Math.random())*2-1);
+
+        //Multiplicar las componentes del vector unitario por la velocidad
+        nvx = nvx*nVelocidad;
+        nvy = nvy*nVelocidad;
+
+		//Añadir al array principal
+        individuos.push({
+          x:Math.round(500*Math.random()),
+          y:Math.round(500*Math.random()),
+          tamano: nTamano,
+          velocidad: nVelocidad,
+          vx: nvx,
+          vy: nvy,
+          vida: nVida,
+		  muerte: Math.floor(Date.now()/1000) + nVida,
+          descendencia: nDescendencia
+        });
+        //Actualiza el listado
+        document.getElementById("listado").innerHTML += "<b>Individuo "+adan+"</b><br>"+
+        "Tama&ntilde;o: "+nTamano+" <progress value=\""+nTamano+"\" max=\"100\"></progress><br>"+
+        "<span style=\"color:red;\">Velocidad: "+nVelocidad+"</span> <progress value=\""+nVelocidad+"\" max=\"100\"></progress><br>"+
+        "<span style=\"color:green;\">Esperanza de vida: "+nVida+"</span> <progress value=\""+nVida+"\" max=\"100\"></progress><br>"+
+        "<span style=\"color:blue;\">Descendencia: "+nDescendencia+"</span> <progress value=\""+nDescendencia+"\" max=\"100\"></progress><br><br>"
+    }	
+}
