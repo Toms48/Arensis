42f9f65cac57dc8947b17dfa5ffdc134cb2c1dcc
Ézhor Malkávar <ezhormalkavar@gmail.com>
2016-11-15 20:29:02 +0100

    Opciones de server SQL cambiadas

--- the-list/opciones.php
+++ the-list/opciones.php
@@ -1,6 +1,6 @@
 <?php
-$mysql_host = "127.0.0.1";
-$mysql_user = "ezhor";
-$mysql_password = "caca95";
-$mysql_database = "lalista";
+$mysql_host = "mysql.hostinger.es";
+$mysql_user = "u363388578_user";
+$mysql_password = "d33]4F@ctk@Lkfy]w2";
+$mysql_database = "u363388578_db";
 ?>
