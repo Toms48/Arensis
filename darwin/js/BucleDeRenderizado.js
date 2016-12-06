abd59cad1fad203fdd17aa39738b3b2fe5e3c947
Ézhor Malkávar <ezhormalkavar@gmail.com>
2016-12-06 15:07:32 +0100

    Descendencia empezada

--- darwin/js/BucleDeRenderizado.js
+++ darwin/js/BucleDeRenderizado.js
@@ -1,64 +1,68 @@
-
-
 //BucleDeRenderizado
-    function loop(individuos, ctx){
-      
-	  //Dibuja el fondo
-      ctx.fillStyle = "#FFFFFF";
-      ctx.fillRect(0,0,800,800);
+function loop(ctx, listado, individuos){
 
-      //Para cada individuo
-      for(var i=0; i<=individuos.length-1; i++){
-		
-        //Define la forma del individuo
-        ctx.beginPath();
-        ctx.arc(individuos[i].x, individuos[i].y, individuos[i].tamano, 0, 2*Math.PI);
+  //Dibuja el fondo
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0,0,800,800);
 
-        //Define su color
-        ctx.fillStyle = "rgb("+Math.round(individuos[i].velocidad*255/100)+","+Math.round(individuos[i].vida*255/100)+","+Math.round(individuos[i].descendencia*255/100)+")";
+  //Para cada individuo
+  for(var i=0; i<=individuos.length-1; i++){
 
-        //Muestra el individuo
-        ctx.fill();
-        ctx.stroke();
-		
-		//Muestra su número
-		ctx.textAlign="center";
-		ctx.textBaseline="middle";
-		ctx.font = "20px Arial"
-		ctx.fillStyle = "#FFFFFF"
-		ctx.fillText(individuos[i].indice,individuos[i].x,individuos[i].y);
-		
-        //Actualiza la posición del individuo
-        individuos[i].x += individuos[i].vx/5;
-        individuos[i].y += individuos[i].vy/5;
+    //Define la forma del individuo
+    ctx.beginPath();
+    ctx.arc(individuos[i].x, individuos[i].y, individuos[i].tamano, 0, 2*Math.PI);
 
-        //Comprueba y activa colisiones con las paredes
-        if(individuos[i].x>=500){
-          individuos[i].x = 500;
-          individuos[i].vx *= -1;
-        }
-        if(individuos[i].x<=0){
-          individuos[i].x = 0;
-          individuos[i].vx *= -1;
-        }
-        if(individuos[i].y>=500){
-          individuos[i].y = 500;
-          individuos[i].vy *= -1;
-        }
-        if(individuos[i].y<=0){
-          individuos[i].y = 0;
-          individuos[i].vy *= -1;
-        }
-		
-		//Comprobar muerte
-		if(individuos[i].muerte <= Math.floor(Date.now()/1000)){
-			individuos.splice(i, 1);
-		}
-		//Reproducción
-		for(var otroIndividuo = i+1; otroIndividuo<=individuos.length-1; otroIndividuo++){
-			if(individuos[i].fertil>Math.floor(Date.now()/1000) && individuos[otroIndividuo].fertil>Math.floor(Date.now()/1000)){
-				individuosArray[adan] = crearIndividuo("listado", indice);
-			}
-		}
-      }
+    //Define su color
+    ctx.fillStyle = "rgb("+Math.round(individuos[i].velocidad*255/100)+","+Math.round(individuos[i].vida*255/100)+","+Math.round(individuos[i].descendencia*255/100)+")";
+
+    //Muestra el individuo
+    ctx.fill();
+    ctx.stroke();
+
+    //Muestra su nï¿½mero
+    ctx.textAlign="center";
+    ctx.textBaseline="middle";
+    ctx.font = "20px Arial"
+    ctx.fillStyle = "#FFFFFF"
+    ctx.fillText(individuos[i].indice,individuos[i].x,individuos[i].y);
+
+    //Actualiza la posiciï¿½n del individuo
+    individuos[i].x += individuos[i].vx/5;
+    individuos[i].y += individuos[i].vy/5;
+
+    //Comprueba y activa colisiones con las paredes
+    if(individuos[i].x>=500){
+      individuos[i].x = 500;
+      individuos[i].vx *= -1;
     }
+    if(individuos[i].x<=0){
+      individuos[i].x = 0;
+      individuos[i].vx *= -1;
+    }
+    if(individuos[i].y>=500){
+      individuos[i].y = 500;
+      individuos[i].vy *= -1;
+    }
+    if(individuos[i].y<=0){
+      individuos[i].y = 0;
+      individuos[i].vy *= -1;
+    }
+
+    //Comprobar muerte
+    if(individuos[i].muerte <= Math.floor(Date.now()/1000)){
+      individuos.splice(i, 1);
+    }
+    //Reproducciï¿½n
+    for(var otroIndividuo = i+1; otroIndividuo<=individuos.length-1; otroIndividuo++){
+      var distanciaCentros = Math.sqrt(Math.pow((individuos[i].x-individuos[otroIndividuo].x),2) + Math.pow((individuos[i].y-individuos[otroIndividuo].y),2));
+      var distanciaReal = distanciaCentros - individuos[i].tamano - individuos[otroIndividuo].tamano;
+
+      if(individuos[i].fertil<Math.floor(Date.now()/1000) && individuos[otroIndividuo].fertil<Math.floor(Date.now()/1000) && distanciaReal<=0){
+        crearIndividuo(listado, individuos);
+        individuos[i].fertil = Math.floor(Date.now()/1000) + (10-(individuos[i].descendencia)/10);
+        individuos[otroIndividuo].fertil = Math.floor(Date.now()/1000) + (10-(individuos[otroIndividuo].descendencia)/10);
+      }
+      console.log("Distancia REAL entre individuo "+i+" e individuo "+otroIndividuo+": "+distanciaReal);
+    }
+  }
+}
--- darwin/js/CrearIndividuo.js
+++ darwin/js/CrearIndividuo.js
@@ -1,113 +1,111 @@
-	/*
-	Prototipo de objeto individuo {x, y, tamano, velocidad, vx, vy, vida, muerte, descendencia, indice, fertil}
-    Las variables tamano, velocidad, vida y descendencia.
-    La suma de esas variables es igual a 100
-    */
-function crearIndividuo(listado, numero , padre, madre){
-	
+/*
+CrearIndividuo
+
+PSEUDOCÓDIGO GENERALIZADO
+inicio
+Aumenta el número de individuos
+Si el individuop no tiene padres
+Asignar atributos aleatorios
+fin_si
+Si_no
+Asignar atributos a partir de los padres
+fin_si_no
+Generar un vector unitario aleatorio para el movimiento
+Obtener vector velocidad
+Crear objeto individuo
+Actualiza el listado
+fin
+
+COMENTARIOS
+Prototipo de objeto individuo {x, y, tamano, velocidad, vx, vy, vida, muerte, descendencia, indice, fertil}
+La suma de las variables tamano, velocidad, vida y descendencia es igual a 100.
+*/
+function crearIndividuo(listado, matriz , padre, madre){
+
 	var atributo = 1;
 
 	var nuevoTamano;
-    var nuevoVelocidad;
-    var nuevoVida;
-    var nuevoDescendencia;
-    var nuevoVx;
+	var nuevoVelocidad;
+	var nuevoVida;
+	var nuevoDescendencia;
+	var nuevoVx;
 	var nuevoVy;
 
-	numero.individuos++;
-	
+	matriz.indice++;
+
 	//Aumenta el número de individuos
-	
-	
+
+
 	//Si el individuo no tiene padres
 	if (padre==undefined || madre==undefined){
-	  //Asignar atributos aleatorios
-      nuevoTamano = 0;
-      nuevoVelocidad = 0;
-      nuevoVida = 0;
-      nuevoDescendencia = 0;
-	  
-      while( (nuevoTamano+nuevoVelocidad+nuevoVida+nuevoDescendencia) < 100 ){
-        switch (atributo) {
-          case 1:
-            nuevoTamano += Math.round(Math.random());
-            atributo++;
-            break;
-          case 2:
-            nuevoVelocidad += Math.round(Math.random());
-            atributo++;
-            break;
-          case 3:
-            nuevoVida += Math.round(Math.random());
-            atributo++;
-            break;
-          case 4:
-            nuevoDescendencia += Math.round(Math.random());
-            atributo++;
-            break;
-          case 5:
-            atributo = 1;
-            break;
+		//Asignar atributos aleatorios
+		nuevoTamano = 0;
+		nuevoVelocidad = 0;
+		nuevoVida = 0;
+		nuevoDescendencia = 0;
 
-        }
-      }
-	  
+		while( (nuevoTamano+nuevoVelocidad+nuevoVida+nuevoDescendencia) < 100 ){
+			switch (atributo) {
+				case 1:
+					nuevoTamano += Math.round(Math.random());
+					atributo++;
+				break;
+				case 2:
+					nuevoVelocidad += Math.round(Math.random());
+					atributo++;
+				break;
+				case 3:
+					nuevoVida += Math.round(Math.random());
+					atributo++;
+				break;
+				case 4:
+					nuevoDescendencia += Math.round(Math.random());
+					atributo++;
+				break;
+				case 5:
+					atributo = 1;
+				break;
+
+			}
+		}
+
 	}
 	//Si_no
 	else{
 		//Asignar atributos a partir de los padres
-		
+
 	}
-	
+
 	//Generar un vector unitario aleatorio para el movimiento
-		//Se multiplica por (Math.round(Math.random())*2-1) para que el número sea positivo o negativo aleatoriomente (lo multiplica por -1 o +1)
+	//Se multiplica por (Math.round(Math.random())*2-1) para que el número sea positivo o negativo aleatoriomente (lo multiplica por -1 o +1)
 	nuevoVx = Math.random() * (Math.round(Math.random())*2-1);
 	nuevoVy = Math.sqrt(1-nuevoVx*nuevoVx) * (Math.round(Math.random())*2-1);
 
-	//Multiplicar las componentes del vector unitario por la velocidad
+	//Obtener vector velocidad
 	nuevoVx = nuevoVx*nuevoVelocidad;
 	nuevoVy = nuevoVy*nuevoVelocidad;
 
 	//Crear objeto individuo
 	individuo = {
-	  x:Math.round(500*Math.random()),
-	  y:Math.round(500*Math.random()),
-	  tamano: nuevoTamano,
-	  velocidad: nuevoVelocidad,
-	  vx: nuevoVx,
-	  vy: nuevoVy,
-	  vida: nuevoVida,
-	  muerte: Math.floor(Date.now()/1000) + nuevoVida,
-	  descendencia: nuevoDescendencia,
-	  indice: numero.individuos,
-	  fertil: 0
+		x:Math.round(500*Math.random()),
+		y:Math.round(500*Math.random()),
+		tamano: nuevoTamano,
+		velocidad: nuevoVelocidad,
+		vx: nuevoVx,
+		vy: nuevoVy,
+		vida: nuevoVida,
+		muerte: Math.floor(Date.now()/1000) + nuevoVida,
+		descendencia: nuevoDescendencia,
+		indice: matriz.indice,
+		fertil: 0
 	};
-	
+
 	//Actualiza el listado
-	document.getElementById(listado).innerHTML += "<b>Individuo "+numero.individuos+"</b><br>"+
+	document.getElementById(listado).innerHTML += "<b>Individuo "+matriz.indice+"</b><br>"+
 	"Tama&ntilde;o: "+nuevoTamano+" <progress value=\""+nuevoTamano+"\" max=\"100\"></progress><br>"+
 	"<span style=\"color:red;\">Velocidad: "+nuevoVelocidad+"</span> <progress value=\""+nuevoVelocidad+"\" max=\"100\"></progress><br>"+
 	"<span style=\"color:green;\">Esperanza de vida: "+nuevoVida+"</span> <progress value=\""+nuevoVida+"\" max=\"100\"></progress><br>"+
 	"<span style=\"color:blue;\">Descendencia: "+nuevoDescendencia+"</span> <progress value=\""+nuevoDescendencia+"\" max=\"100\"></progress><br><br>"
-	
-	return individuo;
-}
 
-	CrearIndividuo
-	
-	PSEUDOCÓDIGO GENERALIZADO
-	inicio
-		Aumenta el número de individuos
-		Si el individuop no tiene padres
-			Asignar atributos aleatorios
-		fin_si
-		Si_no
-			Asignar atributos a partir de los padres
-		fin_si_no
-		
-		Generar un vector unitario aleatorio para el movimiento
-		Multiplicar las componentes del vector unitario por la velocidad
-		Crear objeto individuo
-		Actualiza el listado		
-	fin
-	
+	matriz.push(individuo);
+}
